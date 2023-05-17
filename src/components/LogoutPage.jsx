84cf702f4861c3a2c7af1b62b3913959aacf7464
Jared Boyd <jboyd115@icloud.com>
2023-05-17 17:30:08 +0200

    added in some styles

--- src/components/LogoutPage.jsx
+++ src/components/LogoutPage.jsx
@@ -1,41 +1,42 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import axios from 'axios';
-
+import '../styles/LogoutPage.css'; 
+import videoBg from '../assets/rain.mp4'
 
 function LogoutPage() {
   const [authenticated, setAuthenticated] = useState(true); // Set initial value to true for testing
-  const navigate = useNavigate();
-
-  // const handleLogout = () => {
-  //   // Remove user authentication
-  //   setAuthenticated(false);
-  //   // Remove the cookie
-  //   Cookies.remove('loggedIn');
-  //   // Redirect to login page
-  //   navigate('/');
-  // };
+  
 
   const handleLogout = async () => {
-    await axios.post("https://wind-turbine-app-backend.onrender.com/users/logout", null, {
-      withCredentials: true,
-    }).then((res)=> {
-        console.log('logged out')
-        
-      }).catch ((error) => {
+    await axios
+      .post("https://wind-turbine-app-backend.onrender.com/users/logout", null, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log('logged out');
+      })
+      .catch((error) => {
         console.log(error);
-      }) 
-      setAuthenticated(false);
-      Cookies.remove('token');
-      navigate('/');
-    }
+      });
+    setAuthenticated(false);
+    Cookies.remove('token');
+    navigate('/');
+  };
 
   return (
-    <div>
-      {authenticated && (
-        <button onClick={handleLogout}>Logout</button>
-      )}
+    <div className="logout-page">
+      <div className="video-container">
+      <video src={videoBg} autoPlay loop muted /> 
+      </div>
+      <div className="logout-content text-overlay">
+        <h1>Logout Page</h1>
+        {authenticated && (
+          <button className="logout-button" onClick={handleLogout}>
+            Logout
+          </button>
+        )}
+      </div>
     </div>
   );
 }
