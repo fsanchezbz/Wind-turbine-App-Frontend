26b700f117ca3b9318cc5e98e5336c8b8f6c5579
Jared Boyd <jboyd115@icloud.com>
2023-05-18 10:59:21 +0200

    testing the upload of images

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,9 +2,11 @@ import './rightbar.css';
 import Online from '../online/Online';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+const cloudinary = require('cloudinary').v2;
 
 export default function Rightbar({ profile }) {
   const [users, setUsers] = useState([]);
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -17,6 +19,23 @@ export default function Rightbar({ profile }) {
     };
 
     fetchUserData();
+  }, []);
+
+  useEffect(() => {
+    cloudinary.config({
+      cloud_name: 'windturbineprofile',
+      api_key: '984657238132575',
+      api_secret: 'eTqJxyGsO4-6ZGXTYwF7Wc8KcW8'
+    });
+
+    cloudinary.api.resources({ type: 'upload' })
+      .then((result) => {
+        const images = result.resources;
+        setImages(images);
+      })
+      .catch((error) => {
+        console.error('Error fetching images:', error);
+      });
   }, []);
 
   const HomeRightbar = () => {
@@ -49,6 +68,12 @@ export default function Rightbar({ profile }) {
               <img src={user.profileImage} alt="" className="rightbarFollowingImg" />
               <span className="rightbarFollowingname">{`${user.firstName} ${user.lastName}`}</span>
             </div>
+          ))}
+        </div>
+        <h4 className="rightbarTitle">Uploaded Images</h4>
+        <div className="rightbarImages">
+          {images.map((image) => (
+            <img key={image.public_id} src={image.secure_url} alt="" className="rightbarImage" />
           ))}
         </div>
       </>
