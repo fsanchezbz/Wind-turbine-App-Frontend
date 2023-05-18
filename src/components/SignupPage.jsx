b15f968b8faabdf5dd80ceedf9fb0a7c665621ec
Jared Boyd <jboyd115@icloud.com>
2023-05-18 09:07:48 +0200

    forgot the rest of the components

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -82,12 +82,71 @@ const SignUpPage = () => {
               onChange={handleFileChange}
             />
           </div>
-          {/* Rest of the form */}
+          <div className="form-group">
+            <label className="form-label">Username</label>
+            <input
+              type="text"
+              id="username"
+              className="form-input"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="firstName" className="form-label">
+              First Name
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              className="form-input"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastName" className="form-label">
+              Last Name
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              className="form-input"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="form-input"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="form-input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
           <button type="submit" className="signup-button">
             Sign Up
           </button>
         </form>
-        {/* Rest of the component */}
+        <div className="signup-link">
+          Already have an account? <Link to="/login">Login</Link>
+        </div>
       </div>
     </div>
   );
