f916d3f15adadc0b46ab16636264102cf09cf194
Jared Boyd <jboyd115@icloud.com>
2023-05-18 09:02:37 +0200

    tesing the upload image for profile pics

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -10,44 +10,56 @@ const SignUpPage = () => {
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [profilePicture, setProfilePicture] = useState(null);
-
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    console.log(file[0])
-    setProfilePicture(file);
-  };
+  const [profileImage, setProfileImage] = useState(null); // State for storing the uploaded profile image file
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
+      // Create a new FormData object
       const formData = new FormData();
-      formData.append('userName', userName);
-      formData.append('firstName', firstName);
-      formData.append('lastName', lastName);
-      formData.append('email', email);
-      formData.append('password', password);
-      formData.append('profilePicture', profilePicture);
+      formData.append('profileImage', profileImage); // Append the profile image file to the form data
 
-      const response = await axios.post('https://wind-turbine-app-backend.onrender.com/users/signup', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
+      // Make a request to upload the profile image to Cloudinary
+      const uploadResponse = await axios.post(
+        'https://api.cloudinary.com/v1_1/windturbineprofile/image/upload',
+        formData
+      );
 
-      console.log('User signed up:', response.data);
+      // Get the URL of the uploaded image from the response
+      const profileImageUrl = uploadResponse.data.secure_url;
+
+      // Make a request to sign up the user with the form data
+      const signupResponse = await axios.post(
+        'https://wind-turbine-app-backend.onrender.com/users/signup',
+        {
+          userName: userName,
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          password: password,
+          profileImage: profileImageUrl // Pass the profile image URL in the request
+        }
+      );
+
+      console.log('User signed up:', signupResponse.data);
       // Reset the form fields
       setUserName('');
       setFirstName('');
       setLastName('');
       setEmail('');
       setPassword('');
-      setProfilePicture(null);
+      setProfileImage(null);
       // Redirect to the wind turbines page or perform any other desired action
       window.location.href = '/';
     } catch (error) {
       console.error('Error signing up:', error);
     }
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setProfileImage(file);
   };
 
   return (
@@ -56,82 +68,25 @@ const SignUpPage = () => {
       <div className="signup-container text-overlay">
         <h2 className="signup-title">Sign Up</h2>
         <form className="signup-form" onSubmit={handleSubmit}>
+          {/* Rest of the form */}
           <div className="form-group">
-            <label htmlFor="profilePicture" className="form-label">
+            <label htmlFor="profileImage" className="form-label">
               Profile Picture
             </label>
             <input
               type="file"
-              id="profilePicture"
+              id="profileImage"
               className="form-input"
+              accept="image/*"
               onChange={handleFileChange}
             />
           </div>
-          <div className="form-group">
-            <label className="form-label">Username</label>
-            <input
-              type="text"
-              id="username"
-              className="form-input"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="firstName" className="form-label">
-              First Name
-            </label>
-            <input
-              type="text"
-              id="firstName"
-              className="form-input"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="lastName" className="form-label">
-              Last Name
-            </label>
-            <input
-              type="text"
-              id="lastName"
-              className="form-input"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="form-input"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="form-input"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+          {/* Rest of the form */}
           <button type="submit" className="signup-button">
             Sign Up
           </button>
         </form>
-        <div className="signup-link">
-          Already have an account? <Link to="/login">Login</Link>
-        </div>
+        {/* Rest of the component */}
       </div>
     </div>
   );
