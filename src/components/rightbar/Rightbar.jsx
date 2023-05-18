896fe83b4b3e71916aca473a1b32cd071164f857
Jared Boyd <jboyd115@icloud.com>
2023-05-18 09:38:42 +0200

    reverting back

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -47,14 +47,12 @@ export default function Rightbar({ profile }) {
         <div className="rightbarFollowings">
           {users.map((user) => (
             <div className="rightbarFollowing" key={user.id}>
-              {user.profileImage && (
-                <Image
-                  cloudName="windturbineprofile"
-                  publicId={user.profileImage}
-                  className="rightbarFollowingImg"
-                  alt=""
-                />
-              )}
+              <Image
+                cloudName="windturbineprofile"
+                publicId={user.profileImage}
+                className="rightbarFollowingImg"
+                alt=""
+              />
               <span className="rightbarFollowingname">{`${user.firstName} ${user.lastName}`}</span>
             </div>
           ))}
