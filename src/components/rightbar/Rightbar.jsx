060e28c001829a4ef58a89fee79378e89ba59ada
Jared Boyd <jboyd115@icloud.com>
2023-05-18 11:52:40 +0200

    reverting back

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,7 @@ import Online from '../online/Online';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const Rightbar = ({ profile }) => {
+ const Rightbar = ({ profile, profileImage }) => {
   const [users, setUsers] = useState([]);
   const [images, setImages] = useState([]);
 
@@ -16,16 +16,14 @@ const Rightbar = ({ profile }) => {
         console.error('Error fetching images:', error);
       }
     };
-
+  
     fetchImages();
   }, []);
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const response = await axios.get('https://wind-turbine-app-backend.onrender.com/users/all', {
-          withCredentials: true
-        });
+        const response = await axios.get("https://wind-turbine-app-backend.onrender.com/users/all", { withCredentials: true });
         setUsers(response.data);
       } catch (error) {
         console.log(error);
@@ -39,15 +37,15 @@ const Rightbar = ({ profile }) => {
     return (
       <>
         <div className="birthdayContainer">
-          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <img className='birthdayImg' src="assets/gift.png" alt="" />
           <span className="birthdayText">
             <b>Pola foster</b> and <b>3 others</b> have a birthday today.
           </span>
         </div>
-        <img className="rignhtbarAd" src="assets/ad.png" alt="" />
+        <img className='rignhtbarAd' src="assets/ad.png" alt="" />
         <h4 className="rightbarTitle">Online Friends</h4>
         <ul className="rightbarfriendList">
-          {users.map((u) => (
+          {users.map(u => (
             <Online key={u.id} user={u} />
           ))}
         </ul>
@@ -60,27 +58,30 @@ const Rightbar = ({ profile }) => {
       <>
         <h4 className="rightbarTitle">User information</h4>
         <div className="rightbarFollowings">
-          {users.map((user) => {
-            const userImage = images.find((image) => image.public_id === user.profileImage);
-            return (
-              <div className="rightbarFollowing" key={user.id}>
-                <img src={userImage ? userImage.secure_url : ''} alt="" className="rightbarFollowingImg" />
-                <span className="rightbarFollowingname">{`${user.firstName} ${user.lastName}`}</span>
-              </div>
-            );
-          })}
+          {users.map((user) => (
+            <div className="rightbarFollowing" key={user.id}>
+              <img src={user.profileImage} alt="" className="rightbarFollowingImg" />
+              <span className="rightbarFollowingname">{`${user.firstName} ${user.lastName}`}</span>
+            </div>
+          ))}
         </div>
+        <div>
+    {images.map((image) => (
+      <img key={image.public_id} src={image.secure_url} alt={image.original_filename} />
+    ))}
+  </div>
       </>
     );
   };
 
   return (
-    <div className="rightbar">
+    <div className='rightbar'>
       <div className="rightbarWrapper">
         {profile ? <ProfileRightbar /> : <HomeRightbar />}
       </div>
     </div>
   );
-};
+}
+
 
 export default Rightbar;
