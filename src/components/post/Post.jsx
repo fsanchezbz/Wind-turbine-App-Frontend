7a9700b361a0560a7ba1eaeadf988c71069af439
Jared Boyd <jboyd115@icloud.com>
2023-05-19 17:12:59 +0200

    testing

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,44 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Text, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter, FormControl, FormLabel, Input } from '@chakra-ui/react';
-import axios from 'axios';
-import './post.css';
-
- const Post = () => {
+const Post = () => {
   const [workOrders, setWorkOrders] = useState([]);
-  const [selectedWorkOrderId, setSelectedWorkOrderId] = useState(null); // State for tracking the selected work order
-  const [isOpen, setIsOpen] = useState(false); // State for controlling the modal window
-  const [addInfo, setAddInfo] = useState(''); // State for tracking the entered information
+  const [selectedWorkOrderId, setSelectedWorkOrderId] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [addInfo, setAddInfo] = useState('');
   const [isAdmin, setIsAdmin] = useState(false);
 
-  
   useEffect(() => {
-    const fetchUserData = async () => {
+    const fetchData = async () => {
       try {
         const response = await axios.get("https://wind-turbine-app-backend.onrender.com/users/me", { withCredentials: true });
-        setIsLoggedIn(true);
         setIsAdmin(response.data.isAdmin);
-        console.log(response.data.isAdmin);
       } catch (error) {
         console.log(error);
-        setIsLoggedIn(false);
         setIsAdmin(false);
-        
       }
     };
 
-    fetchUserData();
+    fetchData();
   }, []);
-    
+
   useEffect(() => {
     const fetchWorkOrders = async () => {
       try {
-        const response = await axios.get('https://wind-turbine-app-backend.onrender.com/work/all', 
-        { withCredentials: true });
-       
-        setWorkOrders(response.data)
-
+        const response = await axios.get("https://wind-turbine-app-backend.onrender.com/work/all", { withCredentials: true });
+        setWorkOrders(response.data);
       } catch (error) {
-        console.error('Error fetching work orders:', error);
+        console.error("Error fetching work orders:", error);
       }
     };
 
@@ -58,59 +45,49 @@ import './post.css';
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    // const info = event.target.value 
-    // setInformation(info)
-    
     try {
       const response = await axios.put(`https://wind-turbine-app-backend.onrender.com/work/update/${selectedWorkOrderId}`, {
-         addInfo: addInfo, 
+        addInfo: addInfo,
       });
-      console.log('Work order updated:', response.data);
-      // You can add additional logic here if needed
+      console.log("Work order updated:", response.data);
       closeModal();
     } catch (error) {
-      console.error('Failed to update work order:', error);
+      console.error("Failed to update work order:", error);
     }
   };
 
   const deleteWorkOrder = async (workOrderId) => {
     try {
-      await axios.delete(`https://wind-turbine-app-backend.onrender.com/work/delete/${workOrderId}`);
-      setWorkOrders((prevWorkOrders) =>
-        prevWorkOrders.filter((workOrder) => workOrder._id !== workOrderId)
-      );
+      await axios.delete(`https://wind-turbine-app-backend.onrender.com/work/delete/${workOrderId}`, { withCredentials: true });
+      setWorkOrders((prevWorkOrders) => prevWorkOrders.filter((workOrder) => workOrder._id !== workOrderId));
     } catch (error) {
-      console.error('Failed to delete work order:', error);
+      console.error("Failed to delete work order:", error);
     }
   };
-  
+
   return (
-    
-    <div className='post'>
+    <div className="post">
       <div className="postWrapper">
         <div>
           {workOrders.length > 0 ? (
             workOrders.map((workOrder) => (
               <div key={workOrder._id} className="workOrderCard">
                 <Box>
-                  <Text className='model' marginBottom="0.5rem">
+                  <Text className="model" marginBottom="0.5rem">
                     Turbine Model: {workOrder.turbineModel}
                   </Text>
-                  <Text className='des' marginBottom="0.5rem">
+                  <Text className="des" marginBottom="0.5rem">
                     Description: {workOrder.description}
                   </Text>
-                  <Text className='des' marginBottom="0.5rem">
+                  <Text className="des" marginBottom="0.5rem">
                     Coordinates: {workOrder.location}
                   </Text>
-                  <Text className='tech' marginBottom="0.5rem">
+                  <Text className="tech" marginBottom="0.5rem">
                     Technician: {workOrder.technician}
                   </Text>
-                  <Text className='data'>
-                    {/* Date: {workOrder.date}   */}
-                    Date: {workOrder.date.substring(0, 10)} 
-                  </Text>
+                  <Text className="data">Date: {workOrder.date.substring(0, 10)}</Text>
                   <Button onClick={() => openModal(workOrder._id)}>Add Info</Button>
-                  <Text className='tech' marginBottom="0.5rem">
+                  <Text className="tech" marginBottom="0.5rem">
                     Comments: {workOrder.addInfo}
                   </Text>
                   {isAdmin && (
@@ -118,7 +95,6 @@ import './post.css';
                       Delete
                     </Button>
                   )}
-
                 </Box>
               </div>
             ))
@@ -136,14 +112,15 @@ import './post.css';
               <form onSubmit={handleFormSubmit}>
                 <FormControl>
                   <FormLabel>Information:</FormLabel>
-                  <Input type="text" placeholder="Enter information" value={addInfo}  onChange={(e) => setAddInfo(e.target.value)} />
+                  <Input type="text" placeholder="Enter information" value={addInfo} onChange={(e) => setAddInfo(e.target.value)} />
                 </FormControl>
-                {/* Add more form fields as needed */}
                 <ModalFooter>
                   <Button colorScheme="blue" mr={3} onClick={closeModal}>
                     Close
                   </Button>
-                  <Button type="submit" variant="ghost">Save</Button>
+                  <Button type="submit" variant="ghost">
+                    Save
+                  </Button>
                 </ModalFooter>
               </form>
             </ModalBody>
@@ -152,6 +129,6 @@ import './post.css';
       )}
     </div>
   );
-}
+};
 
 export default Post;
