71e48f419b84b302b5fcf879248c467f2a412203
Jared Boyd <jboyd115@icloud.com>
2023-05-19 11:00:24 +0200

    Testing for pop ups

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter, FormControl, FormLabel, Input } from '@chakra-ui/react';
 import axios from 'axios';
 import './post.css';
 
 export default function Post({ post }) {
   const [workOrders, setWorkOrders] = useState([]);
-  const separatedWorkOrderId = "6464c202a5b48338cd94f1a8"; // ID of the work order to separate
+  const [selectedWorkOrderId, setSelectedWorkOrderId] = useState(null); // State for tracking the selected work order
+  const [isOpen, setIsOpen] = useState(false); // State for controlling the modal window
 
   useEffect(() => {
     const fetchWorkOrders = async () => {
@@ -20,39 +21,75 @@ export default function Post({ post }) {
     fetchWorkOrders();
   }, []);
 
+  const openModal = (workOrderId) => {
+    setSelectedWorkOrderId(workOrderId);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedWorkOrderId(null);
+    setIsOpen(false);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // Perform your form submission logic here
+    closeModal();
+  };
+
   return (
     <div className='post'>
       <div className="postWrapper">
-       
-          <div>
-            {workOrders.length > 0 ? (
-              workOrders.map((workOrder) => (
-                <div key={workOrder._id} className={workOrder._id === separatedWorkOrderId ? "separatedWorkOrderCard" : "workOrderCard"}>
-                  <Box>
-                    <Text className='model' marginBottom="0.5rem">
-                      Turbine Model: {workOrder.turbineModel}
-                    </Text>
-                    <Text className='des' marginBottom="0.5rem">
-                      Description: {workOrder.description}
-                    </Text>
-                    <Text className='des' marginBottom="0.5rem">
-                      Location: {workOrder.location}
-                    </Text>
-                    <Text className='tech' marginBottom="0.5rem">
-                      Technician: {workOrder.technician}
-                    </Text>
-                    <Text className='data'>Date: {workOrder.date}</Text>
-                  </Box>
-                </div>
-              ))
-            ) : (
-              <p>No work orders found.</p>
-            )}
-          </div>
+        <div>
+          {workOrders.length > 0 ? (
+            workOrders.map((workOrder) => (
+              <div key={workOrder._id} className="workOrderCard">
+                <Box>
+                  <Text className='model' marginBottom="0.5rem">
+                    Turbine Model: {workOrder.turbineModel}
+                  </Text>
+                  <Text className='des' marginBottom="0.5rem">
+                    Description: {workOrder.description}
+                  </Text>
+                  <Text className='des' marginBottom="0.5rem">
+                    Location: {workOrder.location}
+                  </Text>
+                  <Text className='tech' marginBottom="0.5rem">
+                    Technician: {workOrder.technician}
+                  </Text>
+                  <Text className='data'>Date: {workOrder.date}</Text>
+                  <Button onClick={() => openModal(workOrder._id)}>Add Info</Button>
+                </Box>
+              </div>
+            ))
+          ) : (
+            <p>No work orders found.</p>
+          )}
         </div>
-        <div className="postBottom">
-        </div>
-      
+      </div>
+      {selectedWorkOrderId && (
+        <Modal isOpen={isOpen} onClose={closeModal}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Add Info</ModalHeader>
+            <ModalBody>
+              <form onSubmit={handleFormSubmit}>
+                <FormControl>
+                  <FormLabel>Information:</FormLabel>
+                  <Input type="text" placeholder="Enter information" />
+                </FormControl>
+                {/* Add more form fields as needed */}
+                <ModalFooter>
+                  <Button colorScheme="blue" mr={3} onClick={closeModal}>
+                    Close
+                  </Button>
+                  <Button type="submit" variant="ghost">Save</Button>
+                </ModalFooter>
+              </form>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      )}
     </div>
   );
 }
