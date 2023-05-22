a714c063cd7c083c820ebd1c4c9c31fea81a22c4
Jared Boyd <jboyd115@icloud.com>
2023-05-22 07:15:58 +0200

    TESTING THE COMMENTS SECTION

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import "./feed.css"
-import Share from '../share/Share'
-import Post from '../post/Post'
-
-import WorkOrder from '../WorkOrder'
 
 export default function Feed() {
   
   return (
     <div className='feed'>
       <div className="feedWrapper">
-        
-          
-          <Post/>
           
       
       </div>
