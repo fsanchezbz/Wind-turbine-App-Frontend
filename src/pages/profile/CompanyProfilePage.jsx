3da675bca7e604ec2354ae6d22a15ec6646a2dfd
Jared Boyd <jboyd115@icloud.com>
2023-05-22 07:34:14 +0200

    Tesing the post comp

--- src/pages/profile/CompanyProfilePage.jsx
+++ src/pages/profile/CompanyProfilePage.jsx
@@ -1,12 +1,15 @@
 import { Box, Flex } from '@chakra-ui/react';
 import Map from '../../components/Map';
 import Post from '../../components/post/Post';
-
+import RightBar from '../../components/rightbar/Rightbar';
 const CompanyProfilePage = () => {
   return (
     <div className="profile">
       <Box marginTop="4rem">
         <Map />
+      </Box>
+      <Box marginTop="4rem">
+        <RightBar />
       </Box>
       <Box marginTop="2rem" padding="0 1rem">
         <Post />
