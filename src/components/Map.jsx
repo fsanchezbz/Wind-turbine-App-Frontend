99ae46dc22f99421ec6533d0847c24fac80515ca
Jared Boyd <jboyd115@icloud.com>
2023-05-23 07:32:59 +0200

    testing the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import { Box, Text, Flex } from '@chakra-ui/react';
 import '../styles/Map.css';
 
-console.log(process.env);
+
 const Map = () => {
   const mapRef = useRef(null);
   const [response, setResponse] = useState('');
