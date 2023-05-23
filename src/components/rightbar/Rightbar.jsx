bb7c0647d2c9bb9f558f9ce35336497981043695
Jared Boyd <jboyd115@icloud.com>
2023-05-23 13:24:46 +0200

    testing the styling

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -40,7 +40,7 @@ const Rightbar = () => {
         <div className="rightbarWrapper">
           <div className="card-deck row row-cols-1 row-cols-md-3">
               {users.map((user) => (
-                <div key={user.id} className="card col mb-3 card h-100" > 
+                <div key={user.id} className="card col mb-3 " > 
                   <div className="card-body" > 
                     <img src={user.profileImage} className="card-img-top" alt="" />
                     <div className="card-body">
