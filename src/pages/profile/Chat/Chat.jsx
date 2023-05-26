9650ca3c46769afd67e986659732341084ea84ca
Jared Boyd <jboyd115@icloud.com>
2023-05-26 19:53:07 +0200

    made the changes to the chat for locations of the chats

--- src/pages/profile/Chat/Chat.jsx
+++ src/pages/profile/Chat/Chat.jsx
@@ -1,34 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 import "./Chat.css";
-
-function Chat({ socket, username, room }) {
+function Chat({ socket, username, room, onCloseChat }) {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
-
-  const sendMessage = async () => {
+  const messageContainerRef = useRef(null);
+  const sendMessage = () => {
     if (currentMessage !== "") {
       const messageData = {
         room: room,
         author: username,
         message: currentMessage,
-        time:
-          new Date(Date.now()).getHours() +
-          ":" +
-          new Date(Date.now()).getMinutes(),
+        time: `${new Date().getHours()}:${new Date().getMinutes()}`,
       };
-
-      await socket.emit("send_message", messageData);
+      socket.emit("send_message", messageData);
       setMessageList((list) => [...list, messageData]);
       setCurrentMessage("");
     }
   };
-
   useEffect(() => {
-    socket.on("receive_message", (data) => {
+    const handleReceiveMessage = (data) => {
       setMessageList((list) => [...list, data]);
-    });
+    };
+    socket.on("receive_message", handleReceiveMessage);
+    return () => {
+      socket.off("receive_message", handleReceiveMessage);
+    };
   }, [socket]);
+  useEffect(() => {
+    // Scroll to the bottom of the message container when a new message is added
+    if (messageContainerRef.current) {
+      messageContainerRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messageList]);
+
 
   return (
     <div className="chat-window">
@@ -37,24 +42,30 @@ function Chat({ socket, username, room }) {
       </div>
       <div className="chat-body">
         <ScrollToBottom className="message-container">
-          {messageList.map((messageContent) => {
-            return (
-              <div
-                className="message"
-                id={username === messageContent.author ? "you" : "other"}
-              >
-                <div>
-                  <div className="message-content">
-                    <p>{messageContent.message}</p>
-                  </div>
-                  <div className="message-meta">
-                    <p id="time">{messageContent.time}</p>
-                    <p id="author">{messageContent.author}</p>
-                  </div>
+          {messageList.map((messageContent) => (
+             <div
+             className="message"
+             id={username === messageContent.author ? "you" : "other"}
+           >
+              <div>
+                <div className="message-content">
+                  <p>{messageContent.message}</p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">{messageContent.time}</p>
+                  <p
+                    id="author"
+                    className={
+                      username === messageContent.author ? "you" : "blue"
+                    }
+                  >
+                    {username === messageContent.author ? "You" : messageContent.author}
+                  </p>
                 </div>
               </div>
-            );
-          })}
+            </div>
+          ))}
+          <div ref={messageContainerRef} />
         </ScrollToBottom>
       </div>
       <div className="chat-footer">
@@ -62,17 +73,15 @@ function Chat({ socket, username, room }) {
           type="text"
           value={currentMessage}
           placeholder="Hey..."
-          onChange={(event) => {
-            setCurrentMessage(event.target.value);
-          }}
+          onChange={(event) => setCurrentMessage(event.target.value)}
           onKeyDown={(event) => {
             event.key === "Enter" && sendMessage();
           }}
         />
         <button onClick={sendMessage}>&#9658;</button>
+
       </div>
     </div>
   );
 }
-
 export default Chat;
