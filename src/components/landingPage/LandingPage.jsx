0087fa51c1ecc5b790e4325141a4fc986f5ac17e
Francisco <fsanchezbz@gmail.com>
2023-06-20 16:56:13 +0200

    LandingPage dont use useNavigate

--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -2,7 +2,7 @@
 import { useTranslation } from 'react-i18next';
 import './LandingPage.css';
 import videoBg from '../../assets/wind.mp4';
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
 
 const LandingPage = () => {
